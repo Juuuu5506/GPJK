04562852f153bfdeb398ec8de3391cb95eb701dd
Justin <justin@HeimPC>
2018-05-12 17:49:53 +0200

    Vielleicht ja jetzt.

---
+++ src/GPMain.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class GPMain {
+	public static void main(String [] args) {
+		System.out.println("A");
+		Scanner sc = new Scanner(System.in);
+		System.out.println(sc.nextLine());
+	}
+}
