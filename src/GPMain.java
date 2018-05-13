cd5cd75c3cd9a858832f7723fbcb45f700785a9a
Justin <justin@HeimPC>
2018-05-13 16:09:24 +0200

    Einlesen von einem Übergebenem in-Dokument.

--- src/GPMain.java
+++ src/GPMain.java
@@ -1,9 +1,18 @@
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class GPMain {
 	public static void main(String [] args) {
 		System.out.println("A");
-		Scanner sc = new Scanner(System.in);
-		System.out.println(sc.nextLine());
+		Scanner sc;
+		try {
+			sc = new Scanner(new File(args[0]));
+			System.out.println(sc.nextLine());
+
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 }
