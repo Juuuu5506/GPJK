664ab1a5684cc8ff9ec1f0ac2f8393ee29b2fe5a
kampmannj <kampmannj@PTJ3011.PTJ-FZJ.KFA-JUELICH.DE>
2018-05-14 16:45:40 +0200

    Die ersten Klassen. Auf der Arbeit.

---
+++ src/Controller.java
@@ -0,0 +1,8 @@
+import java.io.File;
+
+public interface Controller {
+	
+	public void startProgram(File file);
+	
+	public void startAlgo();
+}
---
+++ src/OwnController.java
@@ -0,0 +1,17 @@
+import java.io.File;
+
+public class OwnController implements Controller {
+
+	@Override
+	public void startProgram(File file) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void startAlgo() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
